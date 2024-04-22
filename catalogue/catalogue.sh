1c7ff1289477cd11f83f64ab4717a802f3a6d123
rakesh <brakesh1296@gmail.com>
2024-04-22 12:00:07 +0530

    added

---
+++ catalogue/catalogue.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+dnf module disable nodejs -y
+
+dnf module enable nodejs:18 -y
+
+yum install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+
+cd /app 
+
+unzip /tmp/catalogue.zip
+
+npm install 
+
+cp /home/centos/Shell/catalogue/catalogue.service /etc/systemd/system/catalogue.service
+
+systemctl daemon-reload
+
+systemctl enable catalogue
+
+systemctl start catalogue
+
+cp /home/centos/Shell/catalogue/mongo.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org-shell -y
+
+mongo --host 3.83.165.201 </app/schema/catalogue.js
