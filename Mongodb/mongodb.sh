7d1e312de95dbcf16cece34c46a666e0a9cdafeb
rakesh <brakesh1296@gmail.com>
2024-04-22 11:13:33 +0530

    added

---
+++ Mongodb/mongodb.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org -y
+
+systemctl enable mongod
+
+systemctl start mongod
+
+sed 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+
+systemctl restart mongod
